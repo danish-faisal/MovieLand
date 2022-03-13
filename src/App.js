d6cf45cf9dcce4e1bc1278087317440100b05ae4
= <danishfaisal.wwe@gmail.com>
2022-03-13 21:50:34 +0530

    Homepage setup & MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
 import "./App.css";
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 
 const API_URL = 'http://www.omdbapi.com/?apikey=259341cb';
+
+const movie = {
+  "Title": "Avengers: Endgame",
+  "Year": "2019",
+  "imdbID": "tt4154796",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+};
 
 function App() {
   const searchMovies = async (title) => {
@@ -18,7 +27,17 @@ function App() {
 
 
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input type="text" placeholder="Search for movies" value="Avengers" onChange={() => { }} />
+        <img src={SearchIcon} alt="search" onClick={() => { }} />
+      </div>
+
+      <div className="container">
+        <MovieCard movie={movie} />
+      </div>
+    </div>
   );
 }
 
