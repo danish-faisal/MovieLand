e1535a72d9db3fd0835a1a84093fc3fb46edae92
= <danishfaisal.wwe@gmail.com>
2022-03-13 21:18:26 +0530

    API call written

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+
+
+const API_URL = 'http://www.omdbapi.com/?apikey=259341cb';
+
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('avengers');
+  }, [])
+
+
   return (
     <div>
       App
